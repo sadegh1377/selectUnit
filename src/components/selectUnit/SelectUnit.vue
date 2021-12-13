0a2fffb663578772d22c5543490f9b2dd1c334bd
sadeghhadipour1 <sadeghhadipour1@gmail.com>
2021-12-13 23:33:18 +0330

    fix sumOfUnit bug and add delete icon

--- src/components/selectUnit/SelectUnit.vue
+++ src/components/selectUnit/SelectUnit.vue
@@ -1,177 +1,222 @@
 <template>
-    <div id="BaseTable" class="container">
-        <p>مجموع واحد:{{calculateSum}}</p>
-        <div ref="table">
-            <table ref="table" class="table dir  shadow">
-                <thead class="card-header">
-                <tr>
-                    <th class="dir" v-for="(header, id) in headerOfTable" :key="id">
-                        {{header}}
-                    </th>
-                </tr>
-                </thead>
-                <tr v-for="(cr, index) in courses" :key="index">
-                    <td class="">
-                        <input type="text"
-                               v-focus
-                               v-model="cr.courseName">
-                    </td>
-                    <td>
-                        <input type="text"
-                               v-model="cr.profName">
-                    </td>
-                    <td>
-                        <input type="number" v-model.number="cr.unit" min="0" max="5" :disabled="cr.unit">
-                    </td>
-                    <td v-for="i in numberOfTextArea">
-                        <textarea></textarea>
-                    </td>
-                </tr>
-                <tr class="background pointer" @click="addCrsRow">
-                    <td colspan="9">
-                        <font-awesome-icon icon="plus" size="sm"/>
-                        افزودن ردیف
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <div class="text-left mb-3 ml-3">
-            <span v-if="feedback && courses.length===0" class="text-danger ml-3">{{feedback}}</span>
-            <button class="btn btn-primary shadow" @click="downloadVisualReport">ذخیره کردن</button>
-
-        </div>
+  <div id="BaseTable" class="container">
+    <p>مجموع واحد:{{ sumOfUnits }}</p>
+    <div ref="table">
+      <table ref="table" class="table dir  shadow">
+        <thead class="card-header">
+        <tr>
+          <th class="dir" v-for="(header, id) in headerOfTable" :key="id">
+            {{ header }}
+          </th>
+        </tr>
+        </thead>
+        <tr v-for="(cr, index) in courses" :key="index">
+          <td class="">
+            <input type="text"
+                   v-focus
+                   v-model="cr.courseName">
+          </td>
+          <td>
+            <input type="text"
+                   v-model="cr.profName">
+          </td>
+          <td>
+            <input type="number" v-model.number="cr.unit" min="0" max="5">
+          </td>
+          <td>
+            <textarea v-model="cr.saturday"></textarea>
+          </td>
+          <td>
+            <textarea v-model="cr.sunday"></textarea>
+          </td>
+          <td>
+            <textarea v-model="cr.monday"></textarea>
+          </td>
+          <td>
+            <textarea v-model="cr.tuesday"></textarea>
+          </td>
+          <td>
+            <textarea v-model="cr.wednesday"></textarea>
+          </td>
+          <td>
+            <textarea v-model="cr.examDay"></textarea>
+          </td>
+          <td class="pointer" @click="deleteCrsRow(index)">
+            <font-awesome-icon class="deleteColor" icon="trash"/>
+          </td>
+        </tr>
+        <tr class="background pointer" @click="addCrsRow">
+          <td colspan="12">
+            <font-awesome-icon icon="plus" size="sm"/>
+            افزودن ردیف
+          </td>
+        </tr>
+      </table>
     </div>
+    <div class="text-left mb-3 ml-3">
+      <span v-if="feedback && courses.length===0" class="text-danger ml-3">{{ feedback }}</span>
+      <button class="btn btn-primary shadow" @click="downloadVisualReport">ذخیره کردن</button>
+
+    </div>
+  </div>
 </template>
 
 <script>
-    import html2canvas from "html2canvas"
+import html2canvas from "html2canvas"
 
-    export default {
-        name: "BaseTable",
-        directives: {
-            focus: {
-                inserted(el) {
-                    el.focus()
-                }
-            }
-        },
-        data() {
-            return {
-                courses: [{
-                    profName: null,
-                    courseName: null,
-                    unit: null,
-                }],
-                sumOfUnits: 0,
-                numberOfTextArea: 6,
-                headerOfTable: ["نام درس", "نام استاد", "واحد", "شنبه",
-                    "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "امتحان"],
-                feedback: null,
-
-            }
-        },
-        methods: {
-            deleteRow(index) {
-                this.courses.splice(index, 1);
-            },
-            addCrsRow() {
-                this.courses.push({
-                    profName: null,
-                    courseName: null,
-                    unit: null,
-                })
-            },
-            downloadVisualReport() {
-                window.scrollTo(0, 0);
-                if (this.courses.length === 0) {
-                    this.feedback = "حداقل یک سطر باید پر شود"
-                } else {
-                    html2canvas(this.$refs.table).then(canvas => {
-                        let a = document.createElement('a');
-                        a.href = canvas.toDataURL("image/jpeg")
-                            .replace("image/jpeg", "image/octet-stream");
-                        a.download = 'برنامه درسی.jpg';
-                        a.click();
-                        document.body.removeChild(a);
-                    }).catch((error) => {
-                        console.log(error.toString())
-                    });
-                }
-            },
-        },
-        computed: {
-            calculateSum() {
-                if (this.courses.length > 1 || this.courses[this.courses.length - 1].unit) {
-                    this.sumOfUnits += Number(this.courses[this.courses.length - 1].unit);
-                    return this.sumOfUnits;
-                }
-            }
-        }
+export default {
+  name: "SelectUnit",
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
     }
+  },
+  data() {
+    return {
+      courses: [{
+        profName: null,
+        courseName: null,
+        unit: null,
+        saturday: null,
+        sunday: null,
+        monday: null,
+        tuesday: null,
+        wednesday: null,
+        examDay: null
+
+      }],
+      sumOfUnits: 0,
+      numberOfTextArea: 6,
+      headerOfTable: ["نام درس", "نام استاد", "واحد", "شنبه",
+        "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "امتحان", "حذف"],
+      feedback: null,
+
+    }
+  },
+  methods: {
+    deleteCrsRow(crs) {
+      this.courses.splice(crs, 1);
+    },
+    addCrsRow() {
+      this.courses.push({
+        profName: null,
+        courseName: null,
+        unit: null,
+        saturday: null,
+        sunday: null,
+        monday: null,
+        tuesday: null,
+        wednesday: null,
+        examDay: null
+      })
+    },
+    downloadVisualReport() {
+      window.scrollTo(0, 0);
+      if (this.courses.length === 0) {
+        this.feedback = "حداقل یک سطر باید پر شود"
+      } else {
+        html2canvas(this.$refs.table).then(canvas => {
+          let a = document.createElement('a');
+          a.href = canvas.toDataURL("image/jpeg")
+              .replace("image/jpeg", "image/octet-stream");
+          a.download = 'برنامه درسی.jpg';
+          a.click();
+          document.body.removeChild(a);
+        }).catch((error) => {
+          console.log(error.toString())
+        });
+      }
+    },
+  },
+  watch: {
+    calculateSum(newU, oldU) {
+      let newUnit = JSON.parse(newU)
+      let oldUnit = JSON.parse(oldU)
+      for (let i = 0; i < this.courses.length; i++) {
+        if (newUnit[i].unit !== oldUnit[i].unit) {
+          this.sumOfUnits = Number(this.sumOfUnits) - Number(oldUnit[i].unit) + Number(newUnit[i].unit)
+        }
+      }
+    }
+  },
+  computed: {
+    calculateSum() {
+      return JSON.stringify(this.courses)
+    }
+  }
+}
 </script>
 
 <style scoped>
-    .pointer:hover {
-        cursor: pointer
-    }
 
-    .background {
-        background-color: #f7f7f7;
-    }
+.deleteColor {
+  color: #f84949;
+  margin-top: 20px;
+}
 
-    .dir {
-        text-align: center;
-        direction: rtl;
-    }
+.pointer:hover {
+  cursor: pointer
+}
 
-    p {
-        text-align: center;
-    }
+.background {
+  background-color: #f7f7f7;
+}
 
-    tr:nth-child(odd) {
-        background-color: #f1f1f1;
-    }
+.dir {
+  text-align: center;
+  direction: rtl;
+}
 
-    input {
-      text-align: center;
-      padding: 16px;
-      border: 1px solid #333;
-      border-radius: 10px;
-      outline: none;
-      width: 100%;
-    }
+p {
+  text-align: center;
+}
 
-    .filed {
-        margin-right: auto;
-        margin-left: auto;
-        text-align: center;
-        opacity: 0.8;
-        padding: 16px;
-        /*border: 1px solid #333333;*/
-        /*border-radius: 10px;*/
-        outline: none;
-        width: 210px;
-        overflow: auto;
-    }
+tr:nth-child(odd) {
+  background-color: #f1f1f1;
+}
 
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
+input {
+  text-align: center;
+  padding: 16px;
+  border: 1px solid #333;
+  border-radius: 10px;
+  outline: none;
+  width: 100%;
+}
 
-    input[type=number] {
-      -moz-appearance: textfield;
-      width: 100%!important;
-      border: 1px solid #333;
-      border-radius: 10px;
-      opacity: 1!important;
-    }
+.filed {
+  margin-right: auto;
+  margin-left: auto;
+  text-align: center;
+  opacity: 0.8;
+  padding: 16px;
+  /*border: 1px solid #333333;*/
+  /*border-radius: 10px;*/
+  outline: none;
+  width: 210px;
+  overflow: auto;
+}
 
-    textarea {
-      text-align: center;
-      width: 100%;
-      border: 1px solid #333;
-      border-radius: 10px;
-    }
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type=number] {
+  -moz-appearance: textfield;
+  width: 100% !important;
+  border: 1px solid #333;
+  border-radius: 10px;
+  opacity: 1 !important;
+}
+
+textarea {
+  text-align: center;
+  width: 100%;
+  border: 1px solid #333;
+  border-radius: 10px;
+}
 </style>
